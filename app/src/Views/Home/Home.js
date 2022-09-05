bb240481111296e79b09eafdf407d4c4b9b1ecb1
timmyfight <gtaaa@wp.pl>
2022-09-05 19:08:22 +0200

    Create React App by npx

---
+++ app/src/Views/Home/Home.js
@@ -0,0 +1,12 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <section className={styles.appBody}>
+      <h1>Hello World</h1>
+      <p>New App</p>
+    </section>
+  );
+};
+
+export default Home;
