1c199cda96bbc9eaee20d018ce0ad8b6d2162680
timmyfight <gtaaa@wp.pl>
2022-09-08 23:40:42 +0200

    Create function to rounding temperature, increase font size for temperature, change background in actual weather

--- app/src/Atoms/Temperature/Temperature.js
+++ app/src/Atoms/Temperature/Temperature.js
@@ -1,7 +1,14 @@
 import styles from "./Temperature.module.css";
 
 const Temperature = ({ children }) => {
-  return <p className={styles.temperature}>{children}&#8451;</p>;
+  const roundTemperature = (children) => {
+    let roundedTemperature = parseFloat(children).toFixed(0);
+    return roundedTemperature;
+  };
+
+  return (
+    <p className={styles.temperature}>{roundTemperature(children)}&#8451;</p>
+  );
 };
 
 export default Temperature;
