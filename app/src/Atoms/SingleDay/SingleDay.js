9396688238d42d844969cbb20217b599d69e7ac5
timmyfight <gtaaa@wp.pl>
2022-09-19 18:22:46 +0200

    Implement function to find max and min temperature for day from 3 hours intervals

--- app/src/Atoms/SingleDay/SingleDay.js
+++ app/src/Atoms/SingleDay/SingleDay.js
@@ -9,18 +9,36 @@ const SingleDay = ({ day }) => {
   const getDayName = (fullDate) => {
     return new Date(fullDate).toLocaleString("en-us", { weekday: "long" });
   };
+
+  const findTempDay = (option) => {
+    let min = day[0].main.temp_min;
+    let max = day[0].main.temp_max;
+    for (let i = 0; i < day.length; i++) {
+      if (day[i].main.temp_min < min) {
+        min = day[i].main.temp_min;
+      }
+      if (day[i].main.temp_max > max) {
+        max = day[i].main.temp_max;
+      }
+    }
+    if (option === "min") {
+      return min;
+    } else if (option === "max") {
+      return max;
+    }
+  };
+
   return (
     <section className={styles.dailyForecast}>
       <TitleH2>{getDayName(day[0].dt_txt)}</TitleH2>
-      {console.log("day: ", day)}
       <WeatherIcon
         src={day[4].weather[0].icon}
         alt={day[4].weather[0].description}
       />
       <WeatherDescription>
-        Night <Temperature>{day[0].main.temp_min}</Temperature>
+        Night <Temperature>{findTempDay("min")}</Temperature>
         <Separator type="middleDot" />
-        Day <Temperature>{day[0].main.temp_max}</Temperature>
+        Day <Temperature>{findTempDay("max")}</Temperature>
       </WeatherDescription>
     </section>
   );
