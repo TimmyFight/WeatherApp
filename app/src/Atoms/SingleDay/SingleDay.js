9cfe2915202879789973a1f7f2aa7eb3ccd6ce2f
timmyfight <gtaaa@wp.pl>
2022-09-19 17:56:11 +0200

    Add day name, weather icon and night and day temperatury for 5 days forecast

--- app/src/Atoms/SingleDay/SingleDay.js
+++ app/src/Atoms/SingleDay/SingleDay.js
@@ -1,10 +1,27 @@
 import styles from "./SingleDay.module.css";
+import TitleH2 from "../TitleH2/TitleH2";
+import WeatherIcon from "../WeatherIcon/WeatherIcon";
+import WeatherDescription from "../WeatherDescription/WeatherDescription";
+import Temperature from "../Temperature/Temperature";
+import Separator from "../Separator/Separator";
 
 const SingleDay = ({ day }) => {
+  const getDayName = (fullDate) => {
+    return new Date(fullDate).toLocaleString("en-us", { weekday: "long" });
+  };
   return (
     <section className={styles.dailyForecast}>
-      <h1>{day[0].dt_txt}</h1>
-      <p>{day[0].visibility}</p>
+      <TitleH2>{getDayName(day[0].dt_txt)}</TitleH2>
+      {console.log("day: ", day)}
+      <WeatherIcon
+        src={day[4].weather[0].icon}
+        alt={day[4].weather[0].description}
+      />
+      <WeatherDescription>
+        Night <Temperature>{day[0].main.temp_min}</Temperature>
+        <Separator type="middleDot" />
+        Day <Temperature>{day[0].main.temp_max}</Temperature>
+      </WeatherDescription>
     </section>
   );
 };
