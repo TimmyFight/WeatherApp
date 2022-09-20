9d8a6b129b865332667696fbd20455ff7e1f0f90
timmyfight <gtaaa@wp.pl>
2022-09-20 22:02:40 +0200

    Remove duplicate cities from the recomendation list

--- app/src/Modules/MatchedCities/MatchedCities.js
+++ app/src/Modules/MatchedCities/MatchedCities.js
@@ -2,9 +2,23 @@ import styles from "./MatchedCities.module.css";
 import MatchedCity from "../../Atoms/MatchedCity/MatchedCity";
 
 const MatchedCities = ({ data }) => {
+  const listOfCities = data[0];
+  let indexToRemove = [];
+  let removeDuplicates = listOfCities.map((element) => {
+    return element.name + element.country;
+  });
+
+  for (let i = 0; i < removeDuplicates.length; i++) {
+    if (removeDuplicates.indexOf(removeDuplicates[i]) !== i) {
+      indexToRemove.push(i);
+    }
+  }
+
+  indexToRemove.forEach((element) => delete listOfCities[element]);
+
   return (
     <section className={styles.matchedCities}>
-      {data[0].map((element, index) => {
+      {listOfCities.map((element, index) => {
         return <MatchedCity city={element} key={index} />;
       })}
     </section>
